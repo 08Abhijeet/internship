228033a6dffc2e25353c4c7ab8a78a3516cc0329
Abhijeet <abhijeetkadam656@gmail.com>
2026-02-03 21:48:57 +0530

    updated

--- frontend/src/lib/axiosinstance.js
+++ frontend/src/lib/axiosinstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: process.env.BACKEND_URL || "http://localhost:4000", // Added fallback URL
+  baseURL: process.env.BACKEND_URL, // Added fallback URL
 });
 export default axiosInstance;
